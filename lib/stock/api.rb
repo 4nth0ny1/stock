4abd8c5b86b1afd9d6fa45097d1b7e442f54d201
4anth0ny1 <acatullo4@gmail.com>
2021-01-30 11:47:50 -0600

    Jan 30

--- lib/stock/api.rb
+++ lib/stock/api.rb
@@ -3,27 +3,43 @@ require 'open-uri'
 require 'json'
 require 'pry'
 
+## new plan use only the second api. Change the app to get financial information about the stock. then more information from the same one. 
+## one that is basic information and the second is advanced ratios about the company. 
+## first is company description, address, and employees.
+## second is financial ratios. 
+
+## and if that doesn't work you can pull data from the financial statements and use the stock class to make your own financial ratios instead of another pull from the api. 
+
+
 class API
-
+  ## https://iexcloud.io">Data provided by IEX Cloud
+   
   ## Facebook API
-  URL_facebook = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=FB&interval=5min&outputsize=full&apikey=HJTLZC8RYU1KF8O0"
+  URL_iex_fb_price = "https://cloud.iexapis.com/stable/stock/fb/batch?types=quote,news,chart&range=1m&last=10&token=pk_64a3b816353a4d439a1ab35e94c99d32"
+
+    def get_iex_data_facebook
+      uri = URI.parse(URL_iex_fb_price)
+      response = Net::HTTP.get_response(uri)
+      response.body
+    end
+
+    def get_iex_price_facebook
+      price = JSON.parse(self.get_iex_data_facebook)
+      stock = price["quote"]["companyName"]
+      puts ""
+      puts stock
+      symbol = price["quote"]["symbol"]
+      print "Symbol: "
+      puts symbol
+      latest_price = price["quote"]["latestPrice"]
+      print "Price: "
+      puts latest_price
+      latest_volume = price["quote"]["latestVolume"]
+      print "Volume: "
+      puts latest_volume
+    end
+
   URL_facebook_more_info = "https://www.alphavantage.co/query?function=OVERVIEW&symbol=FB&apikey=HJTLZC8RYU1KF8O0"
-
-  def get_data_facebook
-    uri = URI.parse(URL_facebook)
-    response = Net::HTTP.get_response(uri)
-    response.body
-  end
-
-  def get_price_facebook
-    price = JSON.parse(self.get_data_facebook)
-    stock = price["Time Series (5min)"].first
-    stock[1].each do |type, numbers|
-      if type.include? "close"
-        puts numbers
-      end 
-    end 
-  end
 
   def get_more_facebook_data
     uri = URI.parse(URL_facebook_more_info)
@@ -48,27 +64,33 @@ class API
     eps = more_info["EPS"]
     print "EPS: "
     puts eps
-end
+  end
 
 
 ## Apple API
-URL_Apple = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AAPL&interval=5min&outputsize=full&apikey=HJTLZC8RYU1KF8O0"
 URL_Apple_More_Info = "https://www.alphavantage.co/query?function=OVERVIEW&symbol=AAPL&apikey=HJTLZC8RYU1KF8O0"
+URL_iex_aapl_price = "https://cloud.iexapis.com/stable/stock/aapl/batch?types=quote,news,chart&range=1m&last=10&token=pk_64a3b816353a4d439a1ab35e94c99d32"
 
-def get_data_apple
-  uri = URI.parse(URL_Apple)
+def get_iex_data_apple
+  uri = URI.parse(URL_iex_aapl_price)
   response = Net::HTTP.get_response(uri)
   response.body
 end
 
-def get_price_apple
-  price = JSON.parse(self.get_data_apple)
-  stock = price["Time Series (5min)"].first
-  stock[1].each do |type, numbers|
-    if type.include? "close"
-      puts numbers
-    end 
-  end 
+def get_iex_price_apple
+  price = JSON.parse(self.get_iex_data_apple)
+  stock = price["quote"]["companyName"]
+  puts ""
+  puts stock
+  symbol = price["quote"]["symbol"]
+  print "Symbol: "
+  puts symbol
+  latest_price = price["quote"]["latestPrice"]
+  print "Price: "
+  puts latest_price
+  latest_volume = price["quote"]["latestVolume"]
+  print "Volume: "
+  puts latest_volume
 end
 
 def get_more_data_apple
@@ -97,23 +119,29 @@ def get_more_info_apple
 end
 
   ## Amazon API
-  URL_amazon = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AMZN&interval=5min&outputsize=full&apikey=HJTLZC8RYU1KF8O0"
   URL_amazon_more_info = "https://www.alphavantage.co/query?function=OVERVIEW&symbol=AMZN&apikey=HJTLZC8RYU1KF8O0"
+  URL_iex_amazon_price = "https://cloud.iexapis.com/stable/stock/amzn/batch?types=quote,news,chart&range=1m&last=10&token=pk_64a3b816353a4d439a1ab35e94c99d32"
 
-  def get_data_amazon
-    uri = URI.parse(URL_amazon)
+  def get_iex_data_amazon
+    uri = URI.parse(URL_iex_amazon_price)
     response = Net::HTTP.get_response(uri)
     response.body
   end
-
-  def get_price_amazon
-      price = JSON.parse(self.get_data_amazon)
-      stock = price["Time Series (5min)"].first
-      stock[1].each do |type, numbers|
-      if type.include? "close"
-        puts numbers
-      end 
-    end 
+  
+  def get_iex_price_amazon
+    price = JSON.parse(self.get_iex_data_amazon)
+    stock = price["quote"]["companyName"]
+    puts ""
+    puts stock
+    symbol = price["quote"]["symbol"]
+    print "Symbol: "
+    puts symbol
+    latest_price = price["quote"]["latestPrice"]
+    print "Price: "
+    puts latest_price
+    latest_volume = price["quote"]["latestVolume"]
+    print "Volume: "
+    puts latest_volume
   end
 
   def get_more_data_amazon
@@ -142,23 +170,29 @@ end
   end
 
   ## Netflix API
-  URL_Netflix = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=NFLX&interval=5min&outputsize=full&apikey=HJTLZC8RYU1KF8O0"
   URL_Netflix_More_Info = "https://www.alphavantage.co/query?function=OVERVIEW&symbol=NFLX&apikey=HJTLZC8RYU1KF8O0"
+  URL_iex_netflix_price = "https://cloud.iexapis.com/stable/stock/nflx/batch?types=quote,news,chart&range=1m&last=10&token=pk_64a3b816353a4d439a1ab35e94c99d32"
 
-  def get_data_netflix
-    uri = URI.parse(URL_Netflix)
+  def get_iex_data_netflix
+    uri = URI.parse(URL_iex_netflix_price)
     response = Net::HTTP.get_response(uri)
     response.body
   end
-
-  def get_price_netflix
-      price = JSON.parse(self.get_data_netflix)
-      stock = price["Time Series (5min)"].first
-      stock[1].each do |type, numbers|
-      if type.include? "close"
-        puts numbers
-      end 
-    end 
+  
+  def get_iex_price_netflix
+    price = JSON.parse(self.get_iex_data_netflix)
+    stock = price["quote"]["companyName"]
+    puts ""
+    puts stock
+    symbol = price["quote"]["symbol"]
+    print "Symbol: "
+    puts symbol
+    latest_price = price["quote"]["latestPrice"]
+    print "Price: "
+    puts latest_price
+    latest_volume = price["quote"]["latestVolume"]
+    print "Volume: "
+    puts latest_volume
   end
 
   def get_more_data_netflix
@@ -187,23 +221,29 @@ end
   end
 
     ## Google API
-    URL_Google = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=GOOG&interval=5min&outputsize=full&apikey=HJTLZC8RYU1KF8O0"
     URL_Google_More_Info = "https://www.alphavantage.co/query?function=OVERVIEW&symbol=GOOG&apikey=HJTLZC8RYU1KF8O0"
-  
-    def get_data_google
-      uri = URI.parse(URL_Google)
+    URL_iex_goog_price = "https://cloud.iexapis.com/stable/stock/goog/batch?types=quote,news,chart&range=1m&last=10&token=pk_64a3b816353a4d439a1ab35e94c99d32"
+
+    def get_iex_data_google
+      uri = URI.parse(URL_iex_goog_price)
       response = Net::HTTP.get_response(uri)
       response.body
     end
-  
-    def get_price_google
-        price = JSON.parse(self.get_data_google)
-        stock = price["Time Series (5min)"].first
-        stock[1].each do |type, numbers|
-        if type.include? "close"
-          puts numbers
-        end 
-      end 
+    
+    def get_iex_price_google
+      price = JSON.parse(self.get_iex_data_google)
+      stock = price["quote"]["companyName"]
+      puts ""
+      puts stock
+      symbol = price["quote"]["symbol"]
+      print "Symbol: "
+      puts symbol
+      latest_price = price["quote"]["latestPrice"]
+      print "Price: "
+      puts latest_price
+      latest_volume = price["quote"]["latestVolume"]
+      print "Volume: "
+      puts latest_volume
     end
   
     def get_more_data_google
@@ -231,12 +271,9 @@ end
         puts eps
     end
   
-
-
-
-
-
 end
+
+
 
 # api = API.new
 # api.get_price_facebook
--- lib/stock/cli.rb
+++ lib/stock/cli.rb
@@ -20,17 +20,23 @@ class StockCli::CLI
         puts "4. Netflix"
         puts "5. Google"
         puts ""
-        print "Enter a number (1-5) to select a stock: "
+        print "Enter a number (1-5) to select a stock or type 'exit' to exit the program: "
     end
+
+    def goodbye
+        puts ""
+        puts "Thanks for stopping by."
+        puts ""
+        Kernel.exit!
+    end 
 
     def list_stock
         first_input = gets.strip.downcase
 ## Facebook
         if first_input == "1"
-            print "Facebook, Inc. - "
-            API.new.get_price_facebook
-            puts  "------------------"
-                # more information about facebook
+            API.new.get_iex_price_facebook
+            puts ""
+            # more information about facebook
                 puts "Press y for more information:"
                 input = gets.strip.downcase
                 puts ""
@@ -44,13 +50,13 @@ class StockCli::CLI
                     menu
                     list_stock
                 end
-
+            end
+            
 ## APPLE 
-        elsif first_input == "2"
-            print "Apple, Inc. - "
-            API.new.get_price_apple
-            puts  "------------------"
-               # more information about apple
+        if first_input == "2"
+            API.new.get_iex_price_apple
+            puts ""
+            # more information about apple
                puts "Press y for more information:"
                input = gets.strip.downcase
                puts ""
@@ -64,12 +70,13 @@ class StockCli::CLI
                    menu
                    list_stock
                end
+            end
+            
 ## Amazon
-        elsif first_input == "3"
-            print "Amazon, Inc. - " 
-            API.new.get_price_amazon
-            puts  "------------------"
-                # more information about amazon
+        if first_input == "3"
+            API.new.get_iex_price_amazon
+            puts ""
+            # more information about amazon
                 puts "Press y for more information:"
                 input = gets.strip.downcase
                 puts ""
@@ -83,12 +90,12 @@ class StockCli::CLI
                     menu
                     list_stock
                 end
+            end
 ## Netflix
-        elsif first_input == "4"
-            print "Netflix, Inc. - "
-            API.new.get_price_netflix
-            puts  "------------------"
-                # more information about netflix
+        if first_input == "4"
+            API.new.get_iex_price_netflix
+            puts ""
+            # more information about netflix
                 puts "Press y for more information:"
                 input = gets.strip.downcase
                 puts ""
@@ -102,14 +109,16 @@ class StockCli::CLI
                     menu
                     list_stock
                 end
-
-        elsif first_input == "5"
-            print "Google, Inc. - "
-            API.new.get_price_google
-            puts  "------------------"
-                # more information about google
+            end
+            
+## Google
+        if first_input == "5"
+            API.new.get_iex_price_google
+            puts ""
+            # more information about google
                 puts "Press y for more information:"
                 input = gets.strip.downcase
+
                 puts ""
                 if input == "y"
                     API.new.get_more_info_google
@@ -121,13 +130,20 @@ class StockCli::CLI
                     menu
                     list_stock
                 end
-        else first_input != "1" || "2" || "3" || "4" || "5"
+            end
+        
+        if first_input == "exit"
+            goodbye
+        end 
+        
+        if first_input != "1" || first_input != "2" || first_input != "3" || first_input != "4" || first_input != "5" || first_input != "exit"
+            puts ""
             puts "Invalid Entry. Try again."
             puts ""
             menu
             list_stock
         end 
-    end 
+    end
  
 end
 
